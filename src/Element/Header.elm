62f50e26e7ed4835a64035c6ecc10e8238fb9de7
Marco Martins <marcodanielmartins@gmail.com>
2021-03-03 14:27:22 +0100

    Navigation layout

--- src/Element/Header.elm
+++ src/Element/Header.elm
@@ -1,51 +1,52 @@
 module Element.Header exposing (..)
 
 import Context exposing (ContentContext, Msg)
-import Html exposing (Html, a, div, header, nav, text)
+import Html exposing (Html, a, button, div, header, li, nav, text, ul)
 import Html.Attributes exposing (class, href, rel, target)
 import Pages.PagePath as PagePath exposing (PagePath)
 
 
-
-{--TODO:- mobile version-}
-
-
 headerView : ContentContext -> Html Msg
 headerView { path, frontmatter } =
-    header []
-        [ div [ class "skipNavigation" ]
-            [ a [ href (PagePath.toString path ++ "#mainContent") ] [ text "skip to content" ] ]
-        , nav [ class "navigation" ]
-            [ div [ class "navigation-content container" ]
-                [ div [ class "navigation-content-menu" ]
-                    [ div [ class "navigation-content-menu-brand" ]
+    header [ class "header" ]
+        [ a [ class "header-skip", href (PagePath.toString path ++ "#content") ] [ text "skip to content" ]
+        , div [ class "header-nav container" ]
+            [ nav [ class "header-nav-main" ]
+                [ ul []
+                    [ li [ class "header-nav-main-brand" ]
                         [ a
                             [ href frontmatter.meta.navigation.brand.url ]
                             [ text frontmatter.meta.navigation.brand.text ]
+                        , button [ class "header-nav-main-button" ] [ text "=" ]
                         ]
-                    , div []
-                        (frontmatter.meta.navigation.menu
-                            |> List.map
-                                (\item ->
-                                    a
-                                        [ href item.url
-                                        , class
-                                            (if item.url == ("/" ++ PagePath.toString path) then
-                                                "navigation-content-menu-item active"
-
-                                             else
-                                                "navigation-content-menu-item"
-                                            )
-                                        ]
-                                        [ text item.text ]
-                                )
-                        )
                     ]
-                , div [ class "navigation-content-social" ]
+                , ul [ class "header-nav-main-items" ]
+                    (frontmatter.meta.navigation.menu
+                        |> List.map
+                            (\item ->
+                                li
+                                    [ class
+                                        (if item.url == ("/" ++ PagePath.toString path) then
+                                            "header-nav-item active"
+
+                                         else
+                                            "header-nav-item"
+                                        )
+                                    ]
+                                    [ a [ href item.url ] [ text item.text ] ]
+                            )
+                    )
+                ]
+            , nav [ class "header-nav-extra" ]
+                [ ul []
                     (frontmatter.meta.navigation.social
                         |> List.map
                             (\item ->
-                                a [ href item.url, target "_blank", rel "noopener noreferrer" ] [ text item.text ]
+                                li [ class "header-nav-item" ]
+                                    [ a
+                                        [ href item.url, target "_blank", rel "noopener noreferrer" ]
+                                        [ text item.text ]
+                                    ]
                             )
                     )
                 ]
