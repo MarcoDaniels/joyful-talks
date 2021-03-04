fb074a306f156fc18bd9fbfddb73c59054795bf4
Marco Martins <marcodanielmartins@gmail.com>
2021-03-04 14:29:07 +0100

    Cleanup layout

--- src/Layout.elm
+++ src/Layout.elm
@@ -4,7 +4,7 @@ import Context exposing (ContentContext, Model, Msg(..), PageData)
 import Element.Cookie exposing (cookieView)
 import Element.Footer exposing (footerView)
 import Element.Header exposing (headerView)
-import Html exposing (Html, article)
+import Html exposing (article, div)
 import Html.Attributes exposing (class, id)
 
 
@@ -12,7 +12,7 @@ view : PageData -> ContentContext -> Model -> PageData
 view pageData context model =
     { title = pageData.title
     , body =
-        Html.div [ id "app" ]
+        div [ id "app" ]
             [ headerView context model.menuExpand
             , article [ id "content", class "container" ] [ pageData.body ]
             , cookieView model.cookieConsent context.frontmatter.meta.cookie
