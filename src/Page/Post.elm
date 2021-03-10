17bbec7b692007a8b57fd0bd7389bffbe9424d41
Marco Martins <marcodanielmartins@gmail.com>
2021-03-10 20:03:45 +0100

    Setup post title

--- src/Page/Post.elm
+++ src/Page/Post.elm
@@ -4,12 +4,12 @@ import Context exposing (PageData)
 import Element.Asset exposing (AssetType(..), assetView)
 import Element.Empty exposing (emptyNode)
 import Element.Row exposing (rowView)
-import Html exposing (div)
+import Html exposing (div, h1, text)
 import Html.Attributes exposing (class)
 import Markdown exposing (markdownRender)
 import OptimizedDecoder exposing (Decoder, andThen, field, list, string, succeed)
 import OptimizedDecoder.Pipeline exposing (custom, required)
-import Shared.Decoder exposing (assetDecoder, rowContentDecoder, fieldDecoder)
+import Shared.Decoder exposing (assetDecoder, fieldDecoder, rowContentDecoder)
 import Shared.Types exposing (Field, Post, PostContent, PostContentValue(..))
 
 
@@ -51,22 +51,25 @@ postView : Post -> PageData
 postView post =
     { title = post.title
     , body =
-        Html.div [ Html.Attributes.class "center" ]
-            (post.content
-                |> List.map
-                    (\content ->
-                        case content.value of
-                            PostContentValueMarkdown markdown ->
-                                markdownRender markdown
+        div [ class "post" ]
+            (List.concat
+                [ List.singleton (h1 [ class "post-title font-xl" ] [ text post.title ])
+                , post.content
+                    |> List.map
+                        (\content ->
+                            case content.value of
+                                PostContentValueMarkdown markdown ->
+                                    markdownRender markdown
 
-                            PostContentValueAsset image ->
-                                assetView { src = image.path, alt = "" } AssetPost
+                                PostContentValueAsset image ->
+                                    assetView { src = image.path, alt = "" } AssetPost
 
-                            PostContentValueRow rowItems ->
-                                div [ class "container" ] [ rowView rowItems ]
+                                PostContentValueRow rowItems ->
+                                    rowView rowItems
 
-                            PostContentValueUnknown ->
-                                emptyNode
-                    )
+                                PostContentValueUnknown ->
+                                    emptyNode
+                        )
+                ]
             )
     }
