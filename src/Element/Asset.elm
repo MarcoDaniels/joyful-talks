c14def89ecc72a5c7fc7bba55dafed370d059239
Marco Martins <marcodanielmartins@gmail.com>
2021-03-26 09:27:38 +0100

    Align fonts and assets

--- src/Element/Asset.elm
+++ src/Element/Asset.elm
@@ -1,21 +1,25 @@
-module Element.Asset exposing (AssetType(..), assetView)
+module Element.Asset exposing (AssetDefaultType(..), AssetType(..), assetView)
 
 import Context exposing (Element)
 import Html exposing (img, node, source)
-import Html.Attributes exposing (alt, attribute, media, src)
+import Html.Attributes exposing (alt, attribute, class, media, src)
 
 
 type alias DeviceSizes =
     { s : Int, m : Int, l : Int, xl : Int }
 
 
+type AssetDefaultType
+    = AssetDefaultLandscape
+    | AssetDefaultPortrait
+
+
 type AssetType
     = AssetFeed
-    | AssetPost
     | AssetHero
     | AssetRow Int
     | AssetRelated
-    | AssetDefault
+    | AssetDefault AssetDefaultType
 
 
 type alias Asset =
@@ -26,28 +30,30 @@ assetView : Asset -> AssetType -> Element
 assetView asset assetType =
     case assetType of
         AssetFeed ->
-            imageWithSizes asset { s = 500, m = 300, l = 400, xl = 400 }
+            imageWithSizes asset { s = 500, m = 300, l = 400, xl = 400 } Nothing
 
-        AssetPost ->
-            imageWithSizes asset { s = 500, m = 700, l = 900, xl = 1200 }
+        AssetDefault postType ->
+            case postType of
+                AssetDefaultPortrait ->
+                    imageWithSizes asset { s = 500, m = 700, l = 600, xl = 900 } (Just "img-portrait")
+
+                AssetDefaultLandscape ->
+                    imageWithSizes asset { s = 500, m = 700, l = 900, xl = 1200 } Nothing
 
         AssetHero ->
-            imageWithSizes asset { s = 500, m = 700, l = 900, xl = 1200 }
+            imageWithSizes asset { s = 500, m = 700, l = 900, xl = 1200 } Nothing
 
         AssetRow count ->
-            imageWithSizes asset { s = 500, m = 700, l = 1000 // count, xl = 1200 // count }
+            imageWithSizes asset { s = 500, m = 700, l = 1000 // count, xl = 1200 // count } Nothing
 
         AssetRelated ->
-            imageWithSizes asset { s = 500, m = 700, l = 300, xl = 350 }
-
-        AssetDefault ->
-            imageWithSizes asset { s = 550, m = 750, l = 1000, xl = 1200 }
+            imageWithSizes asset { s = 500, m = 700, l = 300, xl = 350 } Nothing
 
 
-imageWithSizes : Asset -> DeviceSizes -> Element
-imageWithSizes asset sizes =
+imageWithSizes : Asset -> DeviceSizes -> Maybe String -> Element
+imageWithSizes asset sizes className =
     node "picture"
-        []
+        [ class (Maybe.withDefault "" className) ]
         [ source
             [ media "(max-width: 550px)"
             , attribute "sizes" "90vw"
