cc4dfc7d66df910fd251609b2202d5d45f5e9c74
Marco Martins <marcodanielmartins@gmail.com>
2021-03-26 11:52:05 +0100

    Setup robots

---
+++ src/Generate/Robots.elm
@@ -0,0 +1,22 @@
+module Generate.Robots exposing (robots)
+
+import Context exposing (MetadataGenerate)
+
+
+robots : MetadataGenerate -> String
+robots metadata =
+    List.head metadata
+        |> (\maybeMeta ->
+                case maybeMeta of
+                    Just item ->
+                        [ "User-agent: *"
+                        , "Sitemap: "
+                            ++ item.frontmatter.site.baseURL
+                            ++ "/sitemap.xml"
+                        , "Host: " ++ item.frontmatter.site.baseURL
+                        ]
+                            |> String.join "\n"
+
+                    Nothing ->
+                        ""
+           )
