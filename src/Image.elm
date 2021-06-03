dbd691ad11f5954d2b57d960c31dbc6abbee1f5c
Marco Martins <marcodanielmartins@gmail.com>
2021-06-03 20:30:09 +0200

    Update image api with fitToWidth

--- src/Image.elm
+++ src/Image.elm
@@ -5,7 +5,7 @@ imageAPI : String -> Maybe Int -> String
 imageAPI src maybeSrcSet =
     case maybeSrcSet of
         Just srcSet ->
-            "/image/api" ++ src ++ "?w=" ++ String.fromInt srcSet ++ "&o=1&q=60 " ++ String.fromInt srcSet ++ "w"
+            "/image/api" ++ src ++ "?w=" ++ String.fromInt srcSet ++ "&o=1&q=60&m=fitToWidth " ++ String.fromInt srcSet ++ "w"
 
         Nothing ->
-            "/image/api" ++ src ++ "?w=900&o=1&q=60"
+            "/image/api" ++ src ++ "?w=900&o=1&q=60&m=fitToWidth"
